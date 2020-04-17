50b7b6cb6969954884edd6640bc172cf484de575
jerome.hardaway <jerome.hardaway@cbsinteractive.com>
2020-04-16 22:19:51 -0500

    WIP; nned to add cluster stack

--- assets/scripts/matrix.js
+++ assets/scripts/matrix.js
@@ -1,39 +1,75 @@
-// Initialising the canvas
-var canvas = document.querySelector('canvas'),
-  ctx = canvas.getContext('2d');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+  <section id="calling-card">
+    <h1>
+      Jerome Hardaway | Developer Advocate | Educator | Programmer
+    </h1>
+  </section>
+  <section id="main">
+    <div class="intro">
+      <h1>Hi!!!</h1>
+          <img id="headshot" src="assets/img/headshot.jpg" alt="Jerome Head Shot">
+          <p>
+            My name is Jerome Hardaway, and I am a hopeful Developer Advocate for Quicken Loans. After leaving the Air Force at the
+          height of the recession, I retooled myself to become a programmer. While seeing others struggle post-service, I thought
+          to myself, " If it worked for me, it would work for them." So I started teaching other veterans remotely how to program.
+          To date, I have helped over 250 veterans connect and learn this crazy alchemy where we tell the computer what to do
+          called web development. While holding down a job and performing the duties assigned to me, I have been a fierce advocate
+          for training veterans for today's skills NOW, not later, while leading from the front and pushing troops to become
+          better versions of themselves and to master the tools of the digital age.
+        </p>
 
-// Setting the width and height of the canvas
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+          <p>
+            I have performed talks at conferences like OSCON, had the opportunity to write for Stack Overflow, and podcasted as a
+            host on Ruby Rogues. I have showcased the ability to not only educate but entertain while bringing a level of realism
+            into the industry that only one who has seen the world from its worsts views and still found the silver lining can.
+          </p>
+          <p>
+            Front End Architecture, CSS, and Design Sytems make me happy. I hope in the future to lead teams on building front ends
+            that make other programmers envious while helping the business with its goals. The web is a little boring with the
+            coolest toys saved for portfolios. I believe that the world is ready for some spice on the internet, and I hope to one
+            day be the one able to serve it to them without constraints. I find CSS therapeutic, and the idea of making it scalable
+            for large teams excites me.
+          </p>
+          <p>
+            I am on FIRE for Javascript as I told WIRED Magazine, its the working man's language. While accessible to all, it's so
+            wonky and unforgiving that anything can happen. Its the MMA of programming and I love it. I think because, like the
+            military, it has demanded my blood, sweat, and tears.
+          </p>
+          <p>
+            When I am not writing, speaking, teaching, coding, or learning, I'm a family man. I have a wife, three boys and three
+            dogs, all who I am sure are going to be the death of me.
+          </p>
+          <p>
+            If you asked me today why I am the best person for this job, my answer would be simple: right now, I am in the Georges
+            St. Pierre phase of my career. I can add value to the super technical programmers, educate managers and newbies alike,
+            and still hungry to learn and have so much more to value to give the community and help teams become great. So let's do
+            this.
+          </p>
+    </div>
+  </section>
+  </section>
+    <section id="gallery">
 
-// Setting up the letters
-var letters = 'CAPTAINAMERICA';
-letters = letters.split('');
+      <img src="assets/img/colombia-speak.jpg" alt="">
+      <img src="assets/img/combatting-veteran-stigma.jpg" alt="">
+      <img src="assets/img/diversity-in-action.jpg" alt="">
+      <img src="assets/img/newbie-friendly-javascript.jpg" alt="">
+      <img src="assets/img/oscon-big-screen.jpg" alt="">
+      <img src="assets/img/twilio.jpg" alt="">
+      <img src="assets/img/whitehouse.jpg" alt="">
+      <img src="assets/img/wired-magazine.jpg" alt="">
+      <img src="assets/img/with-obama.jpg" alt="">
 
-// Setting up the columns
-var fontSize = 10,
-  columns = canvas.width / fontSize;
-
-// Setting up the drops
-var drops = [];
-for (var i = 0; i < columns; i++) {
-  drops[i] = 1;
-}
-
-// Setting up the draw function
-function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < drops.length; i++) {
-    var text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#0f0';
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-      drops[i] = 0;
-    }
-  }
-}
-
-// Loop the animation
-setInterval(draw, 33);
+    </section>
+  
+</body>
+<script src="assets/scripts/matrix.js"></script>
+</html>
